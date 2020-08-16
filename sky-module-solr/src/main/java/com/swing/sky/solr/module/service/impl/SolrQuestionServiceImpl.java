1063de062ef952a6b418953cbfbbe12c90e9af40
qiu-qian <18845124018@163.com>
2020-08-16 18:03:24 +0800

    使用Solr搜素引擎优化搜素速度

--- sky-module-solr/src/main/java/com/swing/sky/solr/module/service/impl/SolrQuestionServiceImpl.java
+++ sky-module-solr/src/main/java/com/swing/sky/solr/module/service/impl/SolrQuestionServiceImpl.java
@@ -75,8 +75,8 @@ public class SolrQuestionServiceImpl implements SolrQuestionService {
         // 设置高亮显示
         query.setHighlight(true);
         query.addHighlightField("ti_question_content");
-        query.setHighlightSimplePre("<span style='color:red;'>");
-        query.setHighlightSimplePost("</span>");
+        query.setHighlightSimplePre("<text style='color:rgb(253, 174, 100)'>");
+        query.setHighlightSimplePost("</text>");
 
         List<QuestionDO> questions = new ArrayList<>();
         try {
