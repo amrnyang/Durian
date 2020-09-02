5eb9a4c7c950e72bc50893c66d90b9214a529051
swing <18845124018@163.com>
2020-09-02 19:06:45 +0800

    update README.md

---
+++ sky-api-system/src/main/java/com/swing/sky/system/api/tiku/TiAnswerErrorSubmitController.java
@@ -0,0 +1,7 @@
+package com.swing.sky.system.api.tiku;
+
+/**
+ * @author swing
+ */
+public class TiAnswerErrorSubmitController {
+}
---
+++ sky-api-system/src/main/java/com/swing/sky/system/api/tiku/TiQuestionErrorSubmitController.java
@@ -0,0 +1,15 @@
+package com.swing.sky.system.api.tiku;
+
+import io.swagger.annotations.Api;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author swing
+ * @since 2020-9-2
+ */
+@Api
+@Controller
+@RequestMapping("tiku/question")
+public class TiQuestionErrorSubmitController {
+}
